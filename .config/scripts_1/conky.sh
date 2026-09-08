8dc68ebf20ba96a12a16508bf185a07f59d4efb3
zkh <zkh686@gmail.com>
2026-09-08 14:16:59 -0400

    removing unused programs

---
+++ .config/scripts_1/conky.sh
@@ -0,0 +1,6 @@
+
+#how can I get the active compoisitor in the script ? 
+
+hyprctl dispatch workspace 9
+sleep 3
+conky &
---
+++ .config/scripts_1/wallpaper-cache.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+generated_versions="$HOME/.cache/wallpaper-generated"
+rm $generated_versions/*
+echo ":: Wallpaper cache cleared"
+notify-send "Wallpaper cache cleared"
